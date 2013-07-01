d8d065da7be527e4f966274e8715662b526feaa3
Bernhard Mäser <bernhard.maeser@gmail.com>
2013-07-01 18:49:33 +0200

    boilerpalte / dummy implementation

--- vol/main.py
+++ vol/main.py
@@ -1,6 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""vol
+
+volume control in the shell for your mac
+
+Usage:
+    vol (in|out) <volume>
+    vol load <profile>
+
+    vol (-h | --help)
+    vol --version
+
+Options:
+    -h --help     Show this screen.
+    --version     Show version.
+
+"""
+
+
+import sys
+import vol
+
 from docopt import docopt
 
+
+def setvolume(device, amount):
+    # set amount, if mute or full translate to integer
+    print amount
+
 def main():
+    args = docopt(__doc__, version='vol version '+vol.__version__)
+    
+    print args ## todo delete!
+    
+    if args['load'] and args['<profile>']:
+        print args['<profile>']
+        ## todo implement!
+    elif args['out']:
+        setvolume('out', args['<volume>']) 
+    elif args['in']:
+        setvolume('in', args['<volume>'])
+
+
+    sys.exit(0)
