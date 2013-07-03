57d7bc1cec140bc7c3058b5a41a0eaba6fcb7d82
Bernhard Mäser <bernhard.maeser@gmail.com>
2013-07-03 16:21:09 +0200

    muting / unmuting

--- vol/main.py
+++ vol/main.py
@@ -6,8 +6,11 @@
 volume control in the shell for your mac
 
 Usage:
-    vol (in|out) <volume>
+    vol (in|out|alert) <volume>
+    vol mute
+    vol unmute
     vol load <profile>
+    vol info
 
     vol (-h | --help)
     vol --version
@@ -18,29 +21,61 @@ Options:
 
 """
 
-
+import subprocess
 import sys
 import vol # this is ugly, change it
 
 from docopt import docopt
 
 
+def asrun(ascript):
+    "Run the given AppleScript and return the standard output and error."
+
+    ## shamelessly stolen from  
+    ## http://www.leancrew.com/all-this/2013/03/combining-python-and-applescript/
+
+    osa = subprocess.Popen(['osascript', '-'],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE)
+
+    return osa.communicate(ascript)[0]
+
 def setvolume(device, amount):
-    # set amount, if mute or full translate to integer
-    print amount
+    "Set the volume to 'amount' on 'device' "
+
+    cmd ="set volume {0} volume {1}".format(device, amount)
+    print cmd
+
+    pass
+
 
 def main():
+    "Run the main programm."
     args = docopt(__doc__, version='vol version '+vol.__version__)
     
-    print args ## todo delete!
+    #print args ## todo delete!
     
     if args['load'] and args['<profile>']:
         print args['<profile>']
         ## todo implement!
+
+    elif args['info']:
+        print asrun('get volume settings')
+
+    elif args['mute']:
+        asrun('set volume output muted true')
+
+    elif args['unmute']:
+        asrun('set volume output muted false')
+
     elif args['out']:
         setvolume('out', args['<volume>']) 
+
     elif args['in']:
         setvolume('in', args['<volume>'])
+
+    elif args['alert']:
+        setvolume('alert', args['<volume>'])
 
 
     sys.exit(0)
